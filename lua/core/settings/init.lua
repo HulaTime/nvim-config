2d5b33693865fe3999102dc7b9b3c77e73534833
hulatime <hulatime@tutanota.com>
2024-02-07 08:13:30 +0000

    add some qol settings and keymaps

---
+++ lua/core/settings/init.lua
@@ -0,0 +1,26 @@
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.expandtab = true    -- Use spaces instead of tabs
+vim.opt.tabstop = 2         -- A tab is equal to 2 spaces
+vim.opt.shiftwidth = 2      -- Number of spaces to use for each step of (auto)indent
+vim.opt.softtabstop = 2     -- Pressing Tab inserts 2 spaces
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false  -- disable swapfiles
+vim.opt.backup = false    -- I guess disable backups
+vim.opt.undodir = os.getenv('HOME') .. '/.nvim/undodir' -- give the undo plugin long lived access for days and days of stuff
+
+vim.opt.termguicolors = true
+
+vim.scrolloff = 8
+
+vim.updatetime = 50
+
+vim.opt.colorcolumn = '100'
+
+vim.g.mapleader = ' '
+
