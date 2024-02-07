a83ec52269329d2516e84c6ec95bb24befb201b3
hulatime <hulatime@tutanota.com>
2024-02-07 17:06:01 +0000

    a couple yanky maps

--- lua/core/keymaps/init.lua
+++ lua/core/keymaps/init.lua
@@ -5,6 +5,15 @@ require('core.keymaps.telescope')
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
 
+-- quickly navigate between buffers
 vim.keymap.set('n', '<C-H>', ':bprevious<CR>')
 vim.keymap.set('n', '<C-L>', ':bnext<CR>')
+
+-- do not lose yanked content when pasting
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- yank to system clipboard
+vim.keymap.set('n', '<leader>y', "\"+y")
+vim.keymap.set('v', '<leader>y', "\"+y")
+vim.keymap.set('n', '<leader>Y', "\"+Y")
 
