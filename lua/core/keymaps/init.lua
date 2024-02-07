d849c4dc56af2336216f51d05f82a2f4a4f88454
hulatime <hulatime@tutanota.com>
2024-02-07 16:43:33 +0000

    refactor location of keymaps and add maps to navigate buffers

--- lua/core/keymaps/init.lua
+++ lua/core/keymaps/init.lua
@@ -1,3 +1,10 @@
-require('core.keymaps.movement');
+require('core.keymaps.undotree');
 require('core.keymaps.telescope')
+
+-- move highlighted lines up and down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
+
+vim.keymap.set('n', '<C-H>', ':bprevious<CR>')
+vim.keymap.set('n', '<C-L>', ':bnext<CR>')
 
