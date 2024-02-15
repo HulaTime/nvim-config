76339aaabe7672fe65cdc24b8edabe23ee2b50a3
max.provin <provim01@stepstone.com>
2024-02-15 10:11:08 +0000

    wip: add harpoon config

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -5,4 +5,5 @@ require('core.plugin_config.undotree')
 require('core.plugin_config.lsp')
 require('core.plugin_config.fugitive')
 require('core.plugin_config.telescope')
+require('core.plugin_config.harpoon')
 
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,80 +1,84 @@
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-		vim.cmd [[packadd packer.nvim]]
-		return true
-	end
-	return false
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+  use 'wbthomason/packer.nvim'
 
-	-- Functions that everything says you should have :shrug
-	use "nvim-lua/plenary.nvim"
+  -- Functions that everything says you should have :shrug
+  use "nvim-lua/plenary.nvim"
 
-	-- File explorer
-	use 'nvim-tree/nvim-tree.lua'
-	use 'nvim-tree/nvim-web-devicons' -- requires a nerdfont to be installed https://www.nerdfonts.com/
+  -- File explorer
+  use 'nvim-tree/nvim-tree.lua'
+  use 'nvim-tree/nvim-web-devicons' -- requires a nerdfont to be installed https://www.nerdfonts.com/
 
-	-- Builds lexical tree (ast) for language parsing and highlighting etc. 
-	use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
+  -- Builds lexical tree (ast) for language parsing and highlighting etc.
+  use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
 
-	-- Fuzzy find stuff, requires ripgrep to be installed on system for greping
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+  -- Fuzzy find stuff, requires ripgrep to be installed on system for greping
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
-	-- Cos primeagen plugs his shit and it might actually be decent
-	use 'theprimeagen/harpoon'
+  -- Cos primeagen plugs his shit and it might actually be decent
+  use {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { { "nvim-lua/plenary.nvim" } }
+  }
 
-	-- undotree for branching change history that we can jump to
-	use 'mbbill/undotree'
+  -- undotree for branching change history that we can jump to
+  use 'mbbill/undotree'
 
-	-- Git management
-	use 'tpope/vim-fugitive'
+  -- Git management
+  use 'tpope/vim-fugitive'
 
-	-- LSP (using lsp zero for now)
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-			--- Uncomment the two plugins below if you want to manage the language servers from neovim
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+  -- LSP (using lsp zero for now)
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      --- Uncomment the two plugins below if you want to manage the language servers from neovim
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
+    }
+  }
 
   -- Toggle Terminal
-  use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+  use { "akinsho/toggleterm.nvim", tag = '*', config = function()
     require("toggleterm").setup()
-  end}
+  end }
 
-	-- Themes
-	use { 'ribru17/bamboo.nvim', as = 'bamboo-theme' }
-	use { "catppuccin/nvim", as = "catppuccin-theme" }
-	use {
-		'xiantang/darcula-dark.nvim',
-		requires = {"nvim-treesitter/nvim-treesitter"}
-	}
+  -- Themes
+  use { 'ribru17/bamboo.nvim', as = 'bamboo-theme' }
+  use { "catppuccin/nvim", as = "catppuccin-theme" }
+  use {
+    'xiantang/darcula-dark.nvim',
+    requires = { "nvim-treesitter/nvim-treesitter" }
+  }
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if packer_bootstrap then
-		require('packer').sync()
-	end
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end
 )
