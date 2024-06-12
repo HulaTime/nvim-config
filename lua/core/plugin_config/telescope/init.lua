3d8408d7baeb12b55af8c2359f293db132c8ee61
Max Provin <mprovin@motorway.co.uk>
2024-06-12 16:31:39 +0100

    update telescope mappings with a command to grep including hidden files

--- lua/core/plugin_config/telescope/init.lua
+++ lua/core/plugin_config/telescope/init.lua
@@ -1,8 +1,22 @@
 local builtin = require('telescope.builtin')
 
+-- Find project files
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
+-- Find project files that are being tracked by git
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+
+-- Grep over project files, requires ripgrep to be installed
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input('Grep > ') })
+  builtin.grep_string({ search = vim.fn.input('Grep > ') })
 end)
 
+-- Grep over project files including hidden files but excuding git directory, requires ripgrep to be installed
+vim.keymap.set('n', '<leader>hs', function()
+  builtin.grep_string({
+    search = vim.fn.input('Grep > '),
+    additional_args = function()
+      return { '--hidden', '--glob', '!.git/*' }
+    end
+  })
+end)
